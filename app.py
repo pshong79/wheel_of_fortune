49983403dc7b63df55aa54f890f76679cea9d4fa
peter hong <pshong@gmail.com>
2022-04-02 09:01:22 -0500

    added solving puzzle, play_again option

--- app.py
+++ app.py
@@ -12,71 +12,92 @@ def define_player_order():
   return all_players
 
 ##### main program
-consonants = [ 'B', 'C', 'D', 'F', 'G', 'H', 
-               'J', 'K', 'L', 'M', 'N', 'P', 
-               'Q', 'R', 'S', 'T', 'V', 'W', 
-               'X', 'Y', 'Z'
-             ]
-vowels = [ 'A', 'E', 'I', 'O', 'U' ]
+game_on = True
 
-points = [ '100', '100', '100', '100', '100', '100',
-           '200', '200', '200', '200', '200', '200',
-           '500', '500', '500', '750', '750', '750', 
-           '1100', '5000'
-         ]
+while game_on == True:
+  consonants = [ 'B', 'C', 'D', 'F', 'G', 'H', 
+                'J', 'K', 'L', 'M', 'N', 'P', 
+                'Q', 'R', 'S', 'T', 'V', 'W', 
+                'X', 'Y', 'Z'
+              ]
+  vowels = [ 'A', 'E', 'I', 'O', 'U' ]
 
-player1_score = 0
-player2_score = 0
-player3_score = 0
+  points = [ '100', '100', '100', '100', '100', '100',
+            '200', '200', '200', '200', '200', '200',
+            '500', '500', '500', '750', '750', '750', 
+            '1100', '5000'
+          ]
 
-game_over = False
-player1_turn = False
-player2_turn = False
-player3_turn = False
+  player1_score = 0
+  player2_score = 0
+  player3_score = 0
 
-puzzle = input("Enter a word or a phrase for the puzzle.\n")
-puzzle_list = list(puzzle.upper())
-print(puzzle_list)
+  game_over = False
+  player1_turn = False
+  player2_turn = False
+  player3_turn = False
 
-# get a count of each letter in the puzzle_list
-# 1. get length of list
-# 2. iterate through list
-# 3. while not at end of list, 
-# 4. letter1.= item1
-# 5. letter1_count = puzzle_list.count(letter1)
-# 6. if item2 != item1
-# 7. true -> letter2 = item2 and letter2_count = puzzle_list.count(letter2)
-# 8. continue until end of list ()
-
-players = []
-players = define_player_order()
-
-# TODO: need to assign flags to players to manage whose turn it is. 
-
-while game_over == False:
-  print(f"{players[0]}, would you like to:")
   player_options = f"""
-1. Spin the wheel.
-2. Buy a vowel.
-3. Solve the puzzle.
-"""
-  player_choice = input(player_options)
+  1. Spin the wheel.
+  2. Buy a vowel.
+  3. Solve the puzzle.
+  """
 
-  if int(player_choice) == 1:
-    spin_points = random.choice(points)
-    letter = input(f"For {spin_points}, which consonant would you like to choose?\n").upper()
-  elif int(player_choice) == 2:
-    letter = input(f"Which vowel would you like to choose?\n")
-  else:
-    print("Please select a valid option.")
+  players = []
+
+  puzzle = input("Enter a word or a phrase for the puzzle.\n")
+  puzzle_list = list(puzzle.upper())
+  print(puzzle_list)
+
+  # get a count of each letter in the puzzle_list
+  # 1. get length of list
+  # 2. iterate through list
+  # 3. while not at end of list, 
+  # 4. letter1.= item1
+  # 5. letter1_count = puzzle_list.count(letter1)
+  # 6. if item2 != item1
+  # 7. true -> letter2 = item2 and letter2_count = puzzle_list.count(letter2)
+  # 8. continue until end of list ()
+
+  players = define_player_order()
+
+  # TODO: need to assign flags to players to manage whose turn it is. 
+
+  while game_over == False:
+    print(f"{players[0]}, would you like to:")
     player_choice = input(player_options)
 
-  # if # any letter matches:
-  #   # show letters
-  #   # add points
-  #   # ask for options
-  # else # no letters match
-    # next person's turn
+    if int(player_choice) == 1:
+      spin_points = random.choice(points)
+      letter = input(f"For {spin_points} points, which consonant would you like to choose?\n").upper()
+    elif int(player_choice) == 2:
+      letter = input("Which vowel would you like to choose?\n")
+    elif int(player_choice) == 3:
+      attempt = input("What is the puzzle?\n")
+      if attempt.upper() == puzzle.upper():
+        print(f"Congratulations {players[0]}! You solved the puzzle!")
+        game_over = True
+      else:
+        print(f"I'm sorry, {players[0]}, that is incorrect. {players[1]}, it is your turn.")
+    else:
+      print("Please select a valid option.")
+      player_choice = input(player_options)
+
+    # if # any letter matches:
+    #   # show letters
+    #   # add points
+    #   # ask for options
+    # else # no letters match
+      # next person's turn
+
+  play_again = input("Would you like to play again? Y | N ")
+  if play_again.upper() == "Y":
+    game_over = False
+  elif play_again.upper() == "N":
+    break;
+  else:
+    print("Please select a valid option.")
+    play_again = input("Would you like to play again? Y | N ")
 
 
 
